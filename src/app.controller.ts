57e715767df1a69ce98c8045079c3973795b7eb9
Adam Ostapkiewicz <adam.ostapkiewicz@gmail.com>
2018-12-30 14:00:58 +0000

    enviroment setup, and redirect to app when api was called

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res, HttpCode } from '@nestjs/common';
 import { AppService } from './app.service';
+import { homedir } from 'os';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
+
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @HttpCode(301)
+  index(@Res() res: Response) {
+    res.redirect(301, process.env.URL);
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import * as path from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
